3a6de93135ed20e426ee4b448b3b805ac335f7e5
Sahil Thapa <sahil.thapa@gmail.com>
2015-12-24 22:18:23 +0000

    merge sort impl

--- pydevtesting/misc/sortingalgos.py
+++ pydevtesting/misc/sortingalgos.py
@@ -20,16 +20,62 @@ def selectionSort(a):
             tmp = a[i]
             a[i] = a[imin]
             a[imin] = tmp
-    print a        
+    print a
+
+def insertionSort(a):
+    for i in range(len(a)-1):
+        j =  i
+        x = a[i]
+        while j > 0 and a[j-1] > x : 
+            a[j] = a[j-1]
+            j = j-1
+        a[j] = x
+    print a    
+
+def mergeSort(alist):
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ",alist)   
+    
 inp = raw_input("Enter element (add * to quit):")
 y = []
 while (inp != '*'):
     y.append(inp)
     print y
     inp = raw_input("Enter element (add * to quit):")
-print "User Inputted:" 
+print "User Input:" 
 print y 
 print "BubbleSort Output:"
 bubbleSort(y)
 print "SelectionSort Output:"
 selectionSort(y)
+print "InsertionSort Output:"
+selectionSort(y)
+print "MergeSort Output:"
+mergeSort(y)
