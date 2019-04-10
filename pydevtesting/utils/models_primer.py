1dff06fec05699c0963a8642e43e7d813f16b869
sahilthapa <sahil.thapa@gmail.com>
2019-04-10 14:27:28 +0100

    Create models_primer.py

---
+++ pydevtesting/utils/models_primer.py
@@ -0,0 +1,138 @@
+import math
+import numpy
+import random
+import decimal
+import scipy.linalg
+import numpy.random as nrand
+import matplotlib.pyplot as plt
+
+class ModelParameters:
+    """
+    Encapsulates model parameters
+    """
+
+    def __init__(self,
+                 all_s0, all_time, all_delta, all_sigma, gbm_mu,
+                 jumps_lamda=0.0, jumps_sigma=0.0, jumps_mu=0.0,
+                 cir_a=0.0, cir_mu=0.0, all_r0=0.0, cir_rho=0.0,
+                 ou_a=0.0, ou_mu=0.0,
+                 heston_a=0.0, heston_mu=0.0, heston_vol0=0.0):
+        # This is the starting asset value
+        self.all_s0 = all_s0
+        # This is the amount of time to simulate for
+        self.all_time = all_time
+        # This is the delta, the rate of time e.g. 1/252 = daily, 1/12 = monthly
+        self.all_delta = all_delta
+        # This is the volatility of the stochastic processes
+        self.all_sigma = all_sigma
+        # This is the annual drift factor for geometric brownian motion
+        self.gbm_mu = gbm_mu
+        # This is the probability of a jump happening at each point in time
+        self.lamda = jumps_lamda
+        # This is the volatility of the jump size
+        self.jumps_sigma = jumps_sigma
+        # This is the average jump size
+        self.jumps_mu = jumps_mu
+        # This is the rate of mean reversion for Cox Ingersoll Ross
+        self.cir_a = cir_a
+        # This is the long run average interest rate for Cox Ingersoll Ross
+        self.cir_mu = cir_mu
+        # This is the starting interest rate value
+        self.all_r0 = all_r0
+        # This is the correlation between the wiener processes of the Heston model
+        self.cir_rho = cir_rho
+        # This is the rate of mean reversion for Ornstein Uhlenbeck
+        self.ou_a = ou_a
+        # This is the long run average interest rate for Ornstein Uhlenbeck
+        self.ou_mu = ou_mu
+        # This is the rate of mean reversion for volatility in the Heston model
+        self.heston_a = heston_a
+        # This is the long run average volatility for the Heston model
+        self.heston_mu = heston_mu
+        # This is the starting volatility value for the Heston model
+        self.heston_vol0 = heston_vol0
+
+mp = ModelParameters(all_s0=1000,
+                     all_r0=0.5,
+                     all_time=800,
+                     all_delta=0.00396825396,
+                     all_sigma=0.125,
+                     gbm_mu=0.058,
+                     jumps_lamda=0.00125,
+                     jumps_sigma=0.001,
+                     jumps_mu=-0.2,
+                     cir_a=3.0,
+                     cir_mu=0.5,
+                     cir_rho=0.5,
+                     ou_a=3.0,
+                     ou_mu=0.5,
+                     heston_a=0.25,
+                     heston_mu=0.35,
+                     heston_vol0=0.06125)
+
+paths = 15
+
+def plot_stochastic_processes(processes, title):
+    """
+    This method plots a list of stochastic processes with a specified title
+    :return: plots the graph of the two
+    """
+    #plt.style.use(['bmh'])
+    fig, ax = plt.subplots(1)
+    fig.suptitle(title, fontsize=16)
+    ax.set_xlabel('Time, t')
+    ax.set_ylabel('Simulated Asset Price')
+    x_axis = numpy.arange(0, len(processes[0]), 1)
+    for i in range(len(processes)):
+        plt.plot(x_axis, processes[i])
+    plt.show()
+def convert_to_returns(log_returns):
+    """
+    This method exponentiates a sequence of log returns to get daily returns.
+    :param log_returns: the log returns to exponentiated
+    :return: the exponentiated returns
+    """
+    return numpy.exp(log_returns)
+
+
+def convert_to_prices(param, log_returns):
+    """
+    This method converts a sequence of log returns into normal returns (exponentiation) and then computes a price
+    sequence given a starting price, param.all_s0.
+    :param param: the model parameters object
+    :param log_returns: the log returns to exponentiated
+    :return:
+    """
+    returns = convert_to_returns(log_returns)
+    # A sequence of prices starting with param.all_s0
+    price_sequence = [param.all_s0]
+    for i in range(1, len(returns)):
+        # Add the price at t-1 * return at t
+        price_sequence.append(price_sequence[i - 1] * returns[i - 1])
+    return numpy.array(price_sequence)
+
+def brownian_motion_log_returns(param):
+    """
+    This method returns a Wiener process. The Wiener process is also called Brownian motion. For more information
+    about the Wiener process check out the Wikipedia page: http://en.wikipedia.org/wiki/Wiener_process
+    :param param: the model parameters object
+    :return: brownian motion log returns
+    """
+    sqrt_delta_sigma = math.sqrt(param.all_delta) * param.all_sigma
+    return nrand.normal(loc=0, scale=sqrt_delta_sigma, size=param.all_time)
+
+def brownian_motion_levels(param):
+    """
+    Returns a price sequence whose returns evolve according to a brownian motion
+    :param param: model parameters object
+    :return: returns a price sequence which follows a brownian motion
+    """
+    return convert_to_prices(param, brownian_motion_log_returns(param))
+
+if __name__ == "__main__":
+    brownian_motion_examples = []
+    for i in range(paths):
+        brownian_motion_examples.append(brownian_motion_levels(mp))
+
+   # print (brownian_motion_examples)
+    plot_stochastic_processes(brownian_motion_examples, "Brownian Motion")
